07713b0ef0871d3564bad9d65444446d9587f007
beau trepp <beautrepp@gmail.com>
2020-11-10 18:00:20 +0800

    Examples loop nicely
    This resets the camera to the beginning
    once its scaled as far as it needs to

--- examples/forest.rs
+++ examples/forest.rs
@@ -1,10 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::Camera};
 
 struct SpriteScale(Vec3);
+struct ScreenWidth(u32);
 fn main() {
+    let screen_width = ScreenWidth(1088);
     let window = WindowDescriptor {
         title: "Forrest".to_string(),
-        width: 1088,
+        width: screen_width.0,
         height: 640,
         vsync: true,
         resizable: false,
@@ -16,8 +18,10 @@ fn main() {
     App::build()
         .add_resource(window)
         .add_resource(scale)
+        .add_resource(screen_width)
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_system(scroll_camera.system())
         .run();
 }
 
@@ -28,11 +32,11 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let forest_back_trees = asset_server.load("parallax-forest-back-trees.png");
-    let forest_middle_trees= asset_server.load("parallax-forest-middle-trees.png");
+    let forest_middle_trees = asset_server.load("parallax-forest-middle-trees.png");
     let forest_front_trees = asset_server.load("parallax-forest-front-trees.png");
     let forest_lights = asset_server.load("parallax-forest-lights.png");
 
-    let mut sprite = | handle: &Handle<Texture> | -> SpriteComponents {
+    let mut sprite = |handle: &Handle<Texture>| -> SpriteComponents {
         SpriteComponents {
             material: materials.add(handle.clone().into()),
             transform: Transform {
@@ -48,6 +52,16 @@ fn setup(
         .spawn(sprite(&forest_back_trees))
         .spawn(sprite(&forest_lights))
         .spawn(sprite(&forest_middle_trees))
-        .spawn(sprite(&forest_front_trees))
-        ;
+        .spawn(sprite(&forest_front_trees));
+}
+
+/// A simple system that will move the camera to the right
+fn scroll_camera(width: Res<ScreenWidth>, mut query: Query<With<Camera, &mut Transform>>) -> () {
+    let width = (width.0) as f32;
+    for mut transform in query.iter_mut() {
+        *transform.translation.x_mut() += 1.0;
+        if transform.translation.x().floor() > width {
+            *transform.translation.x_mut() = 0.0;
+        }
+    }
 }
